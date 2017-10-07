17263a1d8cffa14050336d33fd90d6663f81a80d
Ameya Rao <ameyarao98@gmail.com>
2017-10-07 17:57:05 +0200

    Finished the sum method
    Did not test it yet

--- src/Finite.java
+++ src/Finite.java
@@ -1,14 +1,23 @@
 import java.util.*;
 
 public class Finite {
-	
+
 	Polynomial sum(Polynomial x, Polynomial y){
-		Polynomial z;
-		for(int i=0;i<=Math.max(x.coeficients.size(), y.coeficients.size());i++){
-			
+		Polynomial z = new Polynomial(null, x.getMod());//we assume that x,y and z(by extention) have the same mod
+		int maxsize = Math.max(x.coefficients.size(), y.coefficients.size());
+		for(int i=maxsize;i>=0;i--){
+			if(i>x.coefficients.size()){
+				z.add1Coefficient(y.coefficients.get(i));
+			}
+			else if(i>y.coefficients.size()){
+				z.add1Coefficient(x.coefficients.get(i));
+			}
+			else{
+				z.add1Coefficient(x.coefficients.get(i) + y.coefficients.get(i));
+			}
 		}
-		
-		
+		return z;
+
 	}
-	
+
 }
