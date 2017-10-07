a763f819b947fe1e37f1524decec94d1b0aca0ad
JoaoFerreira100 <32512196+JoaoFerreira100@users.noreply.github.com>
2017-10-07 17:28:11 +0200

    Changed Polynomial

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,14 +1,19 @@
 import java.util.*;
 
 public class Polynomial {
-	ArrayList<Integer> coeficients;
+	ArrayList<Integer> coefficients;
 	int mod;
 	
 	Polynomial(int[] coef, int m) {
 		mod = m;
 		
 		for (int i = 0; i < coef.length; i++) {
-			coeficients.add(coef[1]);
+			if (coef[i] < 0) {
+				coef[i] = coef[i] + mod*(int)(Math.abs(Math.floor((double)coef[i] / (double))mod)));
+			} else {
+				coef[i] = coef[i] % mod;
+			}
+			coefficients.add(coef[i]);
 		}
 	}
 	
@@ -16,11 +21,40 @@ public class Polynomial {
 		return mod;
 	}
 	
-	ArrayList<Integer> getCoeficients() {
-		return coeficients;
+	ArrayList<Integer> getCoefficients() {
+		return coefficients;
 	}
 	
-	void setNumber(int n) {
-		number = n;
+	int get1Coefficient(int index) {
+		coefficients.get(index);
+	}
+	
+	void add1Coefficient(int element) {
+		if (element < 0) {
+			element = element + mod * (int)(Math.abs(Math.floor( (double) element / (double) mod )));
+		} else {
+			element = element % mod;
+		}
+		coefficients.add(element);
+	}
+	
+	void set1Coefficient(int index,int element) {
+		if (element < 0) {
+			element = element + mod * (int)(Math.abs(Math.floor( (double) element / (double) mod )));
+		} else {
+			element = element % mod;
+		}
+		coefficients.set(index, element);
+	}
+	
+	void setCoefficients(int[] coef) {
+		for (int i = 0; i < coef.length; i++) {
+			if (coef[i] < 0) {
+				coef[i] = coef[i] + mod*(int)(Math.abs(Math.floor((double)coef[i] / (double))mod)));
+			} else {
+				coef[i] = coef[i] % mod;
+			}
+			coefficients.add(coef[i]);
+		}
 	}
 }
